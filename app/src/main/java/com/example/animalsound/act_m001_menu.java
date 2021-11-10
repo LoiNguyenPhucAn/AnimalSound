e2e93fb714ce9b5afe1471300e596ca11ba3525f
LoiNguyenPhucAn <lnphucan@gmail.com>
2021-11-10 10:35:49 +0700

    V1.0_10112021_1030
    build Layout for Portrait and Landscape screen

---
+++ app/src/main/java/com/example/animalsound/act_m001_menu.java
@@ -0,0 +1,14 @@
+package com.example.animalsound;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class act_m001_menu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.act_m001_menu);
+    }
+}
